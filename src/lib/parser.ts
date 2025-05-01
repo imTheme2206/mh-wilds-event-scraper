e410020069765521d82b26492d543fe2e1774071
Theme <worrachit.p@digithunworldwide.com>
2025-05-01 16:33:05 +0700

    fix: parseMHWildsEvents not returning data

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -3,6 +3,7 @@ import scrape from '../scraper';
 
 export async function parseMHWildsEvents(url: string) {
   const data = await scrape(url);
-  const eventDateRangeTab = await getMHWildsEvents(data);
-  console.log(JSON.stringify(eventDateRangeTab, null, 2));
+  const events = await getMHWildsEvents(data);
+
+  return events;
 }
